a215f06daec0d1a24cae081a63fdea171403d9a4
BuildTools <unconfigured@null.spigotmc.org>
2023-11-25 22:08:19 +0800

    hy

---
+++ pages/api/live/analysis.js
@@ -0,0 +1,119 @@
+import CryptoJS from 'crypto-js';
+// import http from 'http'
+
+export default async function handler(req, resp) {
+    console.log('analysis process !!!!')
+    if (req.method === 'GET') {
+        const {roomId} = req.query;
+        const liveUrl = await getFlvV2(roomId);
+        // resp.status(200).send(liveUrl);
+        // resp.redirect(liveUrl);
+
+        resp.writeHead(302, {
+            Location: liveUrl
+        });
+        resp.end();
+
+          // 解析客户端的请求
+        // const { method, url, headers } = req;
+
+         // 发起对直播流的代理请求
+        // http.get(liveUrl.replace('https://', 'http://'), (streamRes) => {
+            // 设置响应头
+            // 将直播流的数据通过代理响应给客户端
+            // streamRes.pipe(resp);
+        // });
+    } else {
+        // 处理其他请求方法
+        resp.status(405).json({ error: 'Method Not Allowed!' });
+    }
+}
+
+
+
+async function getRealRoomId(roomId) {
+    const url = `https://m.huya.com/${roomId}`;
+    const content = await (await fetch(url, {
+            method: 'GET',
+            headers: {
+              'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36' // 添加自定义的标头，比如 Authorization
+            }})).text();
+    console.log(content);
+    const p = /"profileRoom":.*?,/;
+    const m = content.match(p);
+    if (m) {
+        const group = m[0];
+        return group.replace("\"profileRoom\":", "").replace(",", "");
+    }
+    return null;
+}
+
+
+
+async function getFlvV2(roomId) {
+    // const realRoomId = await getRealRoomId(roomId);
+    // console.log(realRoomId);
+    if(!roomId) {
+        throw "roomId 不能为空";
+    }
+   
+    const url = `https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=${roomId}`;
+    const jsonObject = await (await fetch(url)).json();
+    const baseJson = jsonObject.data;
+    if (baseJson.liveStatus.toUpperCase() === "OFF") {
+      return "直播间未开播";
+    }
+  
+    const gid = baseJson.liveData.gid;
+  
+    // realLiveStatus
+    const infoW = baseJson.stream.flv.multiLine[0];
+    const urlW = infoW.url;
+  
+    if ("1663" === gid) {
+      return urlW.replace("http://", "https://");
+    }
+  
+    return flvEncode(urlW);
+  }
+  
+  function flvEncode(urlW) {
+    const split = urlW.split('?');
+    const suffixSplit = split[1].split('&');
+    const map = new Map();
+    suffixSplit.forEach(str => {
+      const strSplit = str.split('=');
+      if (strSplit.length > 1) {
+        map.set(strSplit[0], strSplit[1]);
+      }
+    });
+  
+    let fm = map.get('fm');
+    try {
+      fm = decodeURIComponent(fm);
+    } catch (e) {
+      console.error(e);
+    }
+    const fmBytes = Buffer.from(fm, 'base64');
+    let fmDecode = fmBytes.toString('utf-8');
+    const uid = Date.now();
+    const seqId = uid + Date.now();
+    const wsTime = map.get('wsTime');
+    const type = map.get('ctype');
+    const t = map.get('t');
+    const originalStr = `${seqId}|${type}|${t}`;
+    const hexMd5 = CryptoJS.MD5(originalStr).toString(CryptoJS.enc.Hex);
+    const streamName = split[0].split('.')[3].split('/')[2];
+    fmDecode = fmDecode.replace("\$0", String(uid))
+      .replace("\$1", streamName)
+      .replace("\$2", hexMd5)
+      .replace("\$3", wsTime);
+    const wsSecret = CryptoJS.MD5(fmDecode).toString(CryptoJS.enc.Hex);
+    const txyp = map.get('txyp');
+    const fs = map.get('fs');
+    const sphdcdn = map.get('sphdcdn') || "";
+    const sphdDC = map.get('sphdDC') || "";
+    const uuid = (Date.now() % 10000000000 * 1000 + Math.random() * 1000) % 4294967295;
+    return `${split[0].replace("http", "https")}?wsSecret=${wsSecret}&wsTime=${wsTime}&seqid=${seqId}&ver=1&ctype=${type}&t=${t}&fs=${fs}&txyp=${txyp}&sphdcdn=${sphdcdn}&sphdDC=${sphdDC}&uid=${uid}&uuid=${uuid}&sv=2110211124`;
+  }
+  
---
+++ pages/api/scheduleTask.js
@@ -0,0 +1,77 @@
+
+// const fs = require('fs');
+// const path = require('path');
+// const cron = require('node-cron');
+import fs from 'fs'
+import path from 'path'
+import cron from 'node-cron'
+
+// 返回响应
+export default function handler(req, res) {
+    res.status(200).json({ message: '定时任务已启动' });
+}
+startScheduledTask();
+
+function startScheduledTask() {
+    console.log(1111);
+    exportRecordList();
+    cron.schedule('0 0 */2 * * *', function () {
+        console.log(2222);
+        exportRecordList();
+    });
+}
+
+const baseApiUrl = 'http://localhost:3000/api/live/analysis?roomId=';
+
+async function exportRecordList() {
+
+    let page = 1;
+    const apiUrl = `https://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&gameId=2135&tagAll=0&page=${page}`;
+    const response = await fetch(apiUrl);
+    const responseJson = await response.json();
+    const totalPage = responseJson.data.totalPage;
+    console.log(totalPage);
+    // console.log(JSON.stringify(responseJson));
+    const datas = responseJson.data.datas;
+    const itemList = ["虎牙直播【一起看】,#genre#"]
+    for (const data of datas) {
+        itemList.push(data.introduction + ',' + baseApiUrl + data.profileRoom);
+    }
+    for (page = 2; page <= totalPage; page++) {
+        const response = await fetch(`https://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&gameId=2135&tagAll=0&page=${page}`);
+        const responseJson = await response.json();
+        const datas = responseJson.data.datas;
+        for (const data of datas) {
+            itemList.push(data.introduction + ',' + baseApiUrl + data.profileRoom);
+        }
+    }
+
+    // 目标文件路径
+    const filePath = path.join(process.cwd(), 'public/huya', 'huya.txt');
+
+    // 写入文件
+    fs.writeFile(filePath, itemList.join('\n'), (err) => {
+        if (err) {
+            console.error(err); 
+            return;
+        }
+        console.log('File has been written to public directory');
+        mergeFile();
+    });
+    
+}
+
+async function mergeFile() {
+    // 读取要合并的两个文件
+    const ipv6Str = fs.readFileSync(path.join(process.cwd(), 'public/tv/txt', 'ipv6.txt'), 'utf-8');
+    const huyaStr = fs.readFileSync(path.join(process.cwd(), 'public/huya', 'huya.txt'), 'utf-8');
+
+    // 合并文件内容
+    const mergedContent = ipv6Str + '\n' + huyaStr;
+
+    // 写入新文件
+    const newFilePath = path.join(process.cwd(), 'public/tv/txt', 'all.txt');
+    fs.writeFileSync(newFilePath, mergedContent, 'utf-8');
+
+}
+
